695a689c8ff05418f876c8fc82851672e583914a
AbraCadabstrax <benjamin.blocksom@gmail.com>
2020-05-11 21:53:29 -0400

    Initialize repository with simple login page and web application

---
+++ src/main/java/online/revature/securingspring/SecuringSpringApplication.java
@@ -0,0 +1,13 @@
+package online.revature.securingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringSpringApplication.class, args);
+	}
+
+}
